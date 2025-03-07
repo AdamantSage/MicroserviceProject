65c0c27d2392516510fbc1e8a16f5cb9c3758144
Michael <michaelmathebula009@gmail.com>
2025-03-07 10:50:16 +0200

    pushing initial setup for project with spring security, products page and a createProduct page in progress

---
+++ src/test/java/com/adamant/storemicroservice/StoremicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamant.storemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
