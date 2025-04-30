31e0f6df2d468f812f10003bc9a7c4a8fdee8506
Michael <michaelmathebula009@gmail.com>
2025-04-30 19:25:44 +0200

    added rate limiting

---
+++ src/main/java/com/adamant/storemicroservice/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.adamant.storemicroservice.config;
+
+import com.adamant.storemicroservice.security.RateLimiterInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private RateLimiterInterceptor rateLimiterInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimiterInterceptor)
+                .addPathPatterns("/**"); // Apply to all requests
+    }
+}
